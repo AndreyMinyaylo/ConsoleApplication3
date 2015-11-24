af628411cf8763eb0047963f75fb964bb67ba06c
AndreyMinyaylo <minyailoandrey@gmail.com>
2015-11-24 19:13:42 +0200

    Best My Variant

--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -16,40 +16,28 @@ namespace ConsoleApplication3
             int y = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter number three");
             int z = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number four");
-            int q = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number five");
-            int r = Convert.ToInt32(Console.ReadLine());
-
-
-            if (x > y && x > z&& x > q && x > r)
+            
+            if (x > y)
             {
-               
-                Console.WriteLine("Biggest number is: " + x);
+                if (x > z)
+                {
+                    Console.WriteLine("Biggest number is: " + x);
+                }
             }
-            else if (y > x && y > z && y > q && y > r)
+            if (y > x)
             {
-               
-                Console.WriteLine("Biggest number is: " + y);
+                if (y > z)
+                {
+                    Console.WriteLine("Biggest number is: " + y);
+                }
+                
             }
-            else if (z > y && z > x && z > q && z > r)
+            if (z > x)
             {
-               
-                Console.WriteLine("Biggest number is: " +z);
-            }
-            else if (q > x && q > y && q > z && q > r)
-            {
-
-                Console.WriteLine("Biggest number is: " + q);
-            }
-            else if (r > x && r > y && r > z && r > q)
-            {
-
-                Console.WriteLine("Biggest number is: " + r);
-            }
-            else
-            {
-                Console.WriteLine("Same number is: " + x);
+                if (z > x)
+                {
+                    Console.WriteLine("Biggest number is: " + z);
+                }
             }
             Console.ReadKey();
         } 
