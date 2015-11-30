dd1425562c0234a52c7f80e75a3edced13343437
AndreyMinyaylo <minyailoandrey@gmail.com>
2015-11-30 17:12:12 +0200

    12

---
+++ ConsoleApplication4/TestUnit2/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUnit2
+{
+    public class Class1
+    {
+
+        public void TestMax2()
+        {
+            int x = Convert.ToInt32("5");
+            int y = Convert.ToInt32("8");
+            int z = Convert.ToInt32("7");
+
+            int max;
+
+            max = x;
+
+            if (y > max)
+            {
+                max = y;
+            }
+            if (z > max)
+            {
+                max = z;
+            }
+            Assert.AreEqual(8, max);
+            Debug.Write("max");
+        }
+    }
+    }
