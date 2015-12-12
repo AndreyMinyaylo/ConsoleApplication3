57aa5d4b1edc55f5102b872338f5cddfc8805c3a
AndreyMinyaylo <minyailoandrey@gmail.com>
2015-12-12 20:58:28 +0200

    Tritangle_build_1

---
+++ Diamond_3/Diamond_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  i, j;
+            for (i = 1; i <= 10; i++)
+            {
+                for(j = 1; j <= (10 + i); j++)
+                {
+                    if(j <= (10 - i + 1))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.Write("\r\n");
+            }
+
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+
+     
